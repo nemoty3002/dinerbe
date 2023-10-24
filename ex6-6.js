ac6042614ee18e3364e94f92d5a25ce7acfc3083
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-10-24 09:30:37 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -2,5 +2,5 @@ var who68 = 'world';
 var who66 = '太郎が来た！'; 
 var who67 = '花子が来た！'; 
 function sayhello66() { alert('Hello,'+ who68 +'!'); }
-function taro66() { alert('Hello,'+ who66 +'!'); }
+function taro66() { alert('太郎が来た'); }
 function hanako66() { alert(''+  who67 +'!'); }
