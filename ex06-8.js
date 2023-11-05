442609cf249cdd77d42457817e6d8980f0c3aeac
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-11-05 18:03:44 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,3 +1,3 @@
 function sayhello680() { alert('Hello,' + document.getElementByid('who680').innerHTML + '!'); }
-function taro680() { docment.getElementByld('who680').innerHTML = '太郎'; }
-function hanako680() { docment.getElementByld('who680').innerHTML = '花子'; }
+function taro680() { docment.getElementByid('who680').innerHTML = '太郎'; }
+function hanako680() { docment.getElementByid('who680').innerHTML = '花子'; }
