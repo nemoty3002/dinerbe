3a9c7b9e5dd2927e38998fa851c31ca18e19628e
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-11-07 10:13:02 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -4,7 +4,5 @@ function taro86() { docment.getElementByid('who86').innerHTML = '太郎'; }
 
 function hanako86() { docment.getElementByid('who86').innerHTML = '花子'; } 
 
-
-
 function gazo688()
 {document.getElementByid('who688').innerHTML='<img src="./image/arar.jpg" alt="オン">';}
