aa31c2f3852983722140a857f0f0c519cd5ad553
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-11-07 10:32:33 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,9 +1,12 @@
-function sayhello86() { alert('Hello,'+ document.getElementByid('who86').innerHTML + '!'); }
+function sayhello86()
+  { alert('Hello,' + document.getElementById('who86').innerHTML + '!'); }
 
-function taro86() { document.getElementByid('who86').innerHTML ='太郎'; }
+function taro86() 
+{ document.getElementById('who86').innerHTML ='太郎'; }
 
-function hanako86() { document.getElementByid('who86').innerHTML ='花子'; } 
+function hanako86() 
+{ document.getElementById('who86').innerHTML ='花子'; } 
 
 function gazo688()
-{ document.getElementByid('who688').innerHTML='<img src="./image/arar.jpg" alt="オン">'; }
+{ document.getElementById('who688').innerHTML='<img src="./image/arar.jpg" alt="オン">'; }
 
