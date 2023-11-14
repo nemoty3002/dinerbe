46df456a0c10498a12df4c5973008777f8ce1992
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-11-14 10:22:38 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,2 @@
 function 　imgchange1(){ document.getElementById('logo').src = './image/sns-icon.jpg'; }
-function 　imgchange1(){ document.getElementById('logo').src = './image/logo.jpg'; }
+function 　imgchange2(){ document.getElementById('logo').src = './image/logo.jpg'; }
