285fec4ff9c3f9fed58c4e7af25dbde7c1103a75
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-11-21 09:39:32 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,4 @@
-function 　imgchange1(){ document.getElementById('logo').src = './image/sns-icon.jpg'; }
-function 　imgchange2(){ document.getElementById('logo').src = './image/logo.jpg'; }
+function 　imgchange1(){ document.getElementById('logo').src = './image/hero.jpg'; }
+function 　imgchange2(){ document.getElementById('logo').src = './image/noma.jpg'; }
+function 　imgchange3(){ document.getElementById('logo').src = './image/sns-icon.jpg'; }
+function 　imgchange4(){ document.getElementById('logo').src = './image/logo.jpg'; }
