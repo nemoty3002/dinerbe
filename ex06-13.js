c0554639861df36f7eb3ca44a0224dfe1fa5b2e4
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-11-28 10:18:37 +0900

    Update ex06-13.js

--- ex06-13.js
+++ ex06-13.js
@@ -1,2 +1,15 @@
-function mes() { alert("3秒経ちました！"); }
+function mes() { 
+alert("3秒経ちました！");
+}
+
 function hyoji13() {
+//表示する文字
+var cnt13 = document.timer13.moji13.value.length;
+//文字が全部表示されているか確認
+if ( cnt13 < 11 ) {
+//現在よりも1文字多く切り出して表示
+document.timer13.moji13.value = str13.substr(0,cnt13+1); } else {
+//すべて表示されたら、空文字に戻す
+document.timer13.moji13.value = ""; }
+//setTimeout()を含む関数を呼び出す
+setTimeout("hyoji13()",1000); }  
