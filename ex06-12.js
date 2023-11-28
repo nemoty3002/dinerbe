b9cf1c062cb2bd2e76e90e57d91f232fe14ffb45
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-11-28 09:36:48 +0900

    Update ex06-12.js

--- ex06-12.js
+++ ex06-12.js
@@ -1,5 +1,6 @@
 function hyoji() {
-  //表示する文字　var str = "一文字ずつ表示します。"; 
+//表示する文字
+var str = "一文字ずつ表示します。"; 
 //テキストボックスの文字数
 var cnt = document.timer.moji.value.length;
 //文字が全部表示されているか確認
@@ -7,8 +8,8 @@ if ( cnt < 11 ) {
 //現在よりも1文字多く切り出して表示
 document.timer.moji.value = str.substr(0,cnt+1); }
 else {
-  //全て表示されたら。空文字に戻す
-  document.timer.moji.value = ""; }}
+//全て表示されたら。空文字に戻す
+document.timer.moji.value = ""; }}
 function startfnc() {
-  //関数hyoji()を1000ミリ秒間隔で呼び出す
-setInterval("hyoji()",1000; }
+//関数hyoji()を1000ミリ秒間隔で呼び出す
+setInterval("hyoji()",1000); }
