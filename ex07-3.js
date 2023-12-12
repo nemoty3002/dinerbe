e4b394c5f7e332041bf0648c5a8fe8870f472adb
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2023-12-12 10:07:37 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,1 +1,7 @@
+function keisan(){ //keisanという関数を実行
+  var a, b,d;
+  a = document.getElementById("shincho").value;
+  b	= document.getElementById("taijuu").value; 
+  c = b/( (a/100)*(a/100) ); //BMIの計算結果をcに代入
+　alert('あなたのBMIは' + c + 'です' );}
 
